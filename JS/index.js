a9bb0864e365b79060f2606eb8fd19518d103bfa
KyleCan81 <Kylecan8112@gmail.com>
2024-10-25 15:03:10 -0400

    Trick or Treat v2

--- JS/index.js
+++ JS/index.js
@@ -2,20 +2,27 @@ function handlecandyButtonClick() {
     alert("Candy Button was clicked!")
 }
 
-document.getElementById("candyButton").addEventListener("click", showRandomImage);
-
 const images = [
-    'bubblegum.jpg',
-    'lollipop.jpg',
-    'chocolate.jpg',
-    'candycorn.jpg',
-    'gummybear.jpg',
+    'images/bubblegum.jpg',
+    'images/gummybear.jpg',
+    'images/candycorn.jpg',
+    'images/chocolate.jpg',
+    'images/lollipop.jpg',
 ];
 
 function showRandomImage() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    const imgElement = document.getElementById('randomImage')
+    if (images.length == 0)
+        {
+            //I tried to add an image at the end for the bonus here. 
+            //It'll display the message but had trouble displaying my 6th "empty.jpg" image
+            alert("No more candy for you!!!")
+            imgElement.src = 'images/bubblegum.jpg'
+        }
+    else { 
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const imgElement = document.getElementById('randomImage')
+        imgElement.src = images[randomIndex];
+        imgElement.style.display = 'block';
+        images.splice(randomIndex, 1)
+    }
 }
-
-imgElement.src = images[randomIndex];
-imgElement.style.display = 'block';
